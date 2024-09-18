f243fff5699af7e6a4b0cb93978b59d51f97a9f4
Hardik Modha <hardik.modha27@gmail.com>
2024-09-18 12:06:10 +0530

    FirestoreDependency

--- Package.swift
+++ Package.swift
@@ -3,19 +3,43 @@
 
 import PackageDescription
 
+extension Target.Dependency {
+    
+    public static var tca: Target.Dependency {
+        return .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+    }
+    
+    public static var firebase: Target.Dependency {
+        return .product(name: "FirebaseFirestore", package: "firebase-ios-sdk")
+    }
+}
+
 let package = Package(
     name: "FirestoreDependency",
+    platforms: [.iOS(.v17), .macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FirestoreDependency",
             targets: ["FirestoreDependency"]),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/pointfreeco/swift-composable-architecture.git",
+            .upToNextMajor(from: "1.10.0")),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "11.0.0"))
+        
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FirestoreDependency"),
+            name: "FirestoreDependency",
+            dependencies: [
+                .tca,
+                .firebase
+            ]
+        ),
 
     ]
 )
--- Sources/FirestoreDependency/FirestoreDependency.swift
+++ Sources/FirestoreDependency/FirestoreDependency.swift
@@ -1,2 +1,48 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import ComposableArchitecture
+import Foundation
+import FirebaseCore
+import FirebaseFirestore
+
+public struct FirestorePersistenceKey<T: Codable>: PersistenceReaderKey {
+    var key: String
+    
+    public typealias Value = [T]
+    
+    public init(key: String) {
+        self.key = key
+    }
+    
+    public var id: String {
+        self.key
+    }
+    
+    public func load(initialValue: Value?) -> Value? {
+        return initialValue
+    }
+    
+    public func subscribe(initialValue: Value?, didSet: @escaping (Value?) -> Void) -> Shared<Value>.Subscription {
+        let db = Firestore.firestore().collection(self.key)
+        
+        db.addSnapshotListener { querySanpshots, error in
+            guard let documents = querySanpshots?.documents else {
+                print("No documents")
+                return
+            }
+            let items = documents.compactMap({ try? $0.data(as: T.self)})
+            didSet(items)
+        }
+        
+        return .init {
+            print("cancel")
+        }
+    }
+}
+
+
+
+extension PersistenceReaderKey {
+    public static func firestore<Value>(_ key: String) -> Self where Self == PersistenceKeyDefault<FirestorePersistenceKey<Value>> {
+        
+        return PersistenceKeyDefault(FirestorePersistenceKey(key: key), [])
+    }
+}
